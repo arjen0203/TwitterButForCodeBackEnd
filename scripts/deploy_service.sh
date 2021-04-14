f58643ca0d701ac750bb1f4caee9ec56fbb18538
Arjen <arjenmeer@hotmail.nl>
2021-04-14 14:25:46 +0200

    fixed scrips and frontend docker

--- scripts/deploy_service.sh
+++ scripts/deploy_service.sh
@@ -1,5 +1,5 @@
 echo "## Building Image $1 ##"
-echo "> docker build --tag $1 --file fp/services/$1/deploy/docker/Dockerfile fp/services/$1"
-docker build --tag $1 --file "../fp/services/$1/deploy/docker/Dockerfile" "../fp/services/$1"
+echo "> docker build --tag $1 --file cx/services/$1/deploy/docker/Dockerfile cx/services/$1"
+docker build --tag $1 --file "../cx/services/$1/deploy/docker/Dockerfile" "../cx/services/$1"
 echo "## Deploying $1 ##"
-./deploy_to_kubernetes.sh "../fp/services/$1/deploy/k8s/deployment.yml"
+./deploy_to_kubernetes.sh "../cx/services/$1/deploy/k8s/deployment.yml"
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,4 +1,4 @@
-./set_ingress.sh
 ./build_deploy_all.sh
+./set_ingress.sh
 echo "### Pausing to read possible errors ###"
 sleep 60
