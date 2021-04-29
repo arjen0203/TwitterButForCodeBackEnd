32dc620c11bb147c6a2a4060ee70749dc9c0fdae
Arjen <arjenmeer@hotmail.nl>
2021-04-29 13:01:23 +0200

    added rabbit and github actions test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,4 @@
 kubectl apply -f ../k8s/nginx-ingress-controller.yml
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install rabbit --set auth.username=admin,auth.password=RabbitPassword bitnami/rabbitmq
