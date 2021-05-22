f7a36a28b146dfafa52df530633da5fd7f35cf21
Arjen <arjenmeer@hotmail.nl>
2021-05-22 16:57:16 +0200

    added spring communication protocol amqp

--- cx/services/auth-service/src/main/java/com/arjen0203/codex/service/auth/AuthServiceApplication.java
+++ cx/services/auth-service/src/main/java/com/arjen0203/codex/service/auth/AuthServiceApplication.java
@@ -3,7 +3,6 @@ package com.arjen0203.codex.service.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** Main Auth service application. * */
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
